68768be605135e0ac9d5fd518c310486d2cb220e
shadowfox3735 <normanpan180@gmail.com>
2018-10-24 21:48:18 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,20 @@
+def turn_count(board)
+  counter = 0
+  board_spaces_checked = 0
+  board_spaces = board.size
+  while board_spaces_checked < board_spaces
+    if board[board_spaces_checked] == "X" || board[board_spaces_checked] == "O"
+      counter += 1
+    end
+    board_spaces_checked += 1
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 1
+    "O"
+  else
+    "X"
+  end
+end
