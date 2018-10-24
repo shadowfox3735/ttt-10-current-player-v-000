03a4a8a268c71b6ce2d36cef579237359b65cc20
shadowfox3735 <normanpan180@gmail.com>
2018-10-24 21:40:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,8 @@
 def turn_count(board)
   counter = 0
   board.each do |board|
-    if board.eql? "X" || board.eql? "O"
-      then counter += 1
+    if board == "X" || board == "O"
+      counter += 1
     end
     counter
 end
